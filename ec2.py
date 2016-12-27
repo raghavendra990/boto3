8ef542a4c810cb3136ecb9f2c8204970a685f3fa
root <raghavyadav990@gmail.com>
2016-12-27 17:13:36 +0530

    Optimization, introduse multi threading

--- ec2.py
+++ ec2.py
@@ -1,43 +1,63 @@
 import boto3
 import conf
-
-
+import Queue
+import threading
 
 data = conf.data
+data_keys = conf.keys
 
-
-client = boto3.session.Session(aws_access_key_id="AKIAIQZYFWTIQYP5BDJQ",
-    aws_secret_access_key="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",
-    region_name="ap-southeast-1"
+client = boto3.Session( aws_access_key_id="AKIAIQZYFWTIQYP5BDJQ",
+    aws_secret_access_key="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"
+    
     )
 
-ec2 = client.resource('ec2')
+zone = client.get_available_regions('ec2')
 
-instances = ec2.instances.all()
-print instances 
 non_tag = []
 
-for instance in instances:
-    try:
-        
-        tag = {i["Key"]: i["Value"]  for i in instance.tags}
-        
+def non_tagged(j, client, q):
+    ec2 =  client.resource("ec2" , region_name=j)
+
+    instances = ec2.instances.all() 
+    for instance in instances:
         try:
-            tag["owner"]
-
-            keys = data.keys()
-
-            for i in keys:
-                if tag[i] in data[i]:
-                    pass
-                else:
-                    non_tag.append(instance.id)
-                    break
             
-        except KeyError as e:
-            non_tag.append(instance.id)
+            tag = {i["Key"]: i["Value"]  for i in instance.tags}
+            
+            try:
+                [tag[i] for i in data_keys]
 
-    except TypeError:
-        non_tag.append(instance.id)
-    
+
+                keys = data.keys()
+
+                for i in keys:
+                    if tag[i] in data[i]:
+                        pass
+                    else:
+                        q.put({"instance_id":instance.id , "region":j})
+                        break
+                
+            except KeyError as e:
+                q.put({"instance_id":instance.id , "region":j})
+
+        except TypeError:
+            q.put({"instance_id":instance.id , "region":j})
+
+q = Queue.Queue()
+
+threads= []
+
+for j in zone:
+    t = threading.Thread(target=non_tagged, args = (j, client, q))
+    threads.append(t)
+    t.start()
+
+for x in threads:
+    x.join()
+
+while(True):
+    try:
+        non_tag.append(q.get(block=False))
+    except Queue.Empty:
+        break
 print non_tag
--- ec2_volumes.py
+++ ec2_volumes.py
@@ -1,49 +1,50 @@
 import boto3
 import conf
-
+import threading 
 
 data = conf.data
-data_keys = conf.keys
 
 client = boto3.Session( aws_access_key_id="AKIAIQZYFWTIQYP5BDJQ",
     aws_secret_access_key="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"
     
     )
 
-zone = client.get_available_regions('ec2')
+zone = client.get_available_regions(service_name='ec2'  )
 
-non_tag = []
 
-for j in zone:
-    instances =  client.resource("ec2" , region_name=j).instances.all() 
-    # instances = instances.append( client.resource("ec2" , region_name=j).instances.all() 
+def tag_volumes(j, client):
+
+    ec2 =  client.resource("ec2" , region_name=j)
+
+    instances = ec2.instances.all()
+
+    
 
     for instance in instances:
         print instance.id
-        try:
-            
-            tag = {i["Key"]: i["Value"]  for i in instance.tags}
-            
-            try:
-                [tag[i] for i in data_keys]
+        volumes = instance.volumes.all()
+        tag = instance.tags
+        if tag != None:
+            for v in volumes:
+                v.create_tags(Tags=tag)
 
 
-                keys = data.keys()
-
-                for i in keys:
-                    if tag[i] in data[i]:
-                        pass
-                    else:
-                        non_tag.append(instance.id)
-                        break
-                
-            except KeyError as e:
-                non_tag.append(instance.id)
-
-        except TypeError:
-            non_tag.append(instance.id)
-
-print non_tag
 
 
+for j in zone:
+    t  = threading.Thread(target= tag_volumes , args = (j,client))
+    t.start()
+
+# for j in zone:
+#     ec2 =  client.resource("ec2" , region_name=j)
+
+#     instances = ec2.instances.all()
+
+#     for instance in instances:
+#         print instance.id
+#         volumes = instance.volumes.all()
+#         tag = instance.tags
+#         if tag != None:
+#             for v in volumes:
+#                 v.create_tags(Tags=tag)
 
